557865ea5758c4f9d5f3dca8c0700cba107fbeaa
E5883 <pedro.h.silva@itsector.pt>
2020-04-15 21:50:22 +0100

    Adicionado um splashscreen ao iniciar a aplicação.

---
+++ app/src/main/java/com/example/android/diadata/ui/SplashScreen.java
@@ -0,0 +1,31 @@
+package com.example.android.diadata.ui;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.android.diadata.MainActivity;
+import com.example.android.diadata.R;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 5000);
+
+    }
+
+}
