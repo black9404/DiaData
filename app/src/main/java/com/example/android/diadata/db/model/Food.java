515e0f7871621f343dda71f3ebfd674194b1e4b0
E5883 <pedro.h.silva@itsector.pt>
2020-05-10 14:58:21 +0100

    Atualizados os ficheiros da bdd.

--- app/src/main/java/com/example/android/diadata/db/model/Food.java
+++ app/src/main/java/com/example/android/diadata/db/model/Food.java
@@ -1,4 +1,21 @@
 package com.example.android.diadata.db.model;
 
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "food")
 public class Food {
+
+    @PrimaryKey(autoGenerate = true)
+    int id_alimento;
+    @ColumnInfo(name = "nome")
+    String nome;
+    @ColumnInfo(name = "hidratos")
+    String hidratos;
+    @ColumnInfo(name = "tipo_alimento")
+    boolean tipo_alimento;
+    @ColumnInfo(name = "quantidade_alimento")
+    int quantidade_alimeto;
+
 }
