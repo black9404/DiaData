bcf5c511d64a3977a1db742f0ce35e420e9f01ee
E5883 <pedro.h.silva@itsector.pt>
2020-05-10 14:52:43 +0100

    Adicionados mais ficheiros da bdd.

---
+++ app/src/main/java/com/example/android/diadata/db/dao/FoodDao.java
@@ -0,0 +1,4 @@
+package com.example.android.diadata.db.dao;
+
+public class FoodDao {
+}
--- app/src/main/java/com/example/android/diadata/db/model/User.java
+++ app/src/main/java/com/example/android/diadata/db/model/User.java
@@ -2,24 +2,41 @@ package com.example.android.diadata.db.model;
 
 import androidx.room.ColumnInfo;
 import androidx.room.Entity;
+import androidx.room.Ignore;
 import androidx.room.PrimaryKey;
 
 @Entity(tableName = "user")
 public class User {
 
     @PrimaryKey(autoGenerate = true)
-    int id;
+    private int id;
     @ColumnInfo(name = "nome")
-    String nome;
+    private String nome;
     @ColumnInfo(name = "subnome")
-    String subnome;
+    private String subnome;
     @ColumnInfo(name = "idade")
-    int idade;
+    private int idade;
     @ColumnInfo(name = "tipo_diabetes")
-    boolean tipo_diabetes;
+    private boolean tipo_diabetes;
     @ColumnInfo(name = "idade_diabetes")
-    int idade_diabetes;
+    private int idade_diabetes;
 
+    public User(int id, String nome, String subnome, int idade, boolean tipo_diabetes, int idade_diabetes) {
+        this.id = id;
+        this.nome = nome;
+        this.subnome = subnome;
+        this.idade = idade;
+        this.tipo_diabetes = tipo_diabetes;
+        this.idade_diabetes = idade_diabetes;
+    }
 
+    @Ignore
+    public User(String nome, String subnome, int idade, boolean tipo_diabetes, int idade_diabetes) {
+        this.nome = nome;
+        this.subnome = subnome;
+        this.idade = idade;
+        this.tipo_diabetes = tipo_diabetes;
+        this.idade_diabetes = idade_diabetes;
+    }
 
 }
