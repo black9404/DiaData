158ad53f7c72a6f30af9753d5413f0e45d65d0f5
Pinheiro <46682218+p1nh3iro@users.noreply.github.com>
2020-06-10 20:23:16 +0100

    Código para adição do icon para o fragmento do perfil(fragment_profile.xml)

--- app/src/main/java/com/example/android/diadata/ui/Dashboard.java
+++ app/src/main/java/com/example/android/diadata/ui/Dashboard.java
@@ -1,22 +1,28 @@
 package com.example.android.diadata.ui;
 
 import android.app.Notification;
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
 import com.example.android.diadata.MainActivity;
 import com.example.android.diadata.R;
+import com.google.android.material.bottomappbar.BottomAppBar;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.Objects;
@@ -26,13 +32,13 @@ import static com.example.android.diadata.core.App.CHANNEL_ID;
 public class Dashboard extends Fragment {
 
     private NotificationManagerCompat notificationManager;
-
     private TextView userNameTextView;
     private FloatingActionButton addFloatingActionButton;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.dashboard, container, false);
+        View v = inflater.inflate(R.layout.dashboard, container, false);
+        return v;
     }
 
     @Override
@@ -45,13 +51,13 @@ public class Dashboard extends Fragment {
     //metodo que instancia todos os elementos presentes no layout
     private void initViews() {
         userNameTextView = Objects.requireNonNull(getView()).findViewById(R.id.userNameComplete);
-
         addFloatingActionButton = Objects.requireNonNull(getView()).findViewById(R.id.fab);
         addFloatingActionButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 testNotifications();
                 addData();
+                openProfile();
             }
         });
 
@@ -98,4 +104,27 @@ public class Dashboard extends Fragment {
 
     }
 
+    private void openProfile() {
+        BottomAppBar bottomNav = getActivity().findViewById(R.id.bar);
+        bottomNav.setOnMenuItemClickListener(navListener);
+
+        getFragmentManager().beginTransaction().replace(R.id.fragment_container, new Profile()).commit();
+    }
+
+    private BottomAppBar.OnMenuItemClickListener navListener = new BottomAppBar.OnMenuItemClickListener() {
+        @Override
+        public boolean onMenuItemClick(MenuItem item) {
+            Fragment selectedFragment = new Profile();
+            switch(item.getItemId()){
+                case R.id.navProfile:
+                    selectedFragment = new Profile();
+                    break;
+                case R.id.navNotifications:
+                    selectedFragment = new Profile();
+                    break;
+            }
+            getFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
+            return true;
+        }
+    };
 }
