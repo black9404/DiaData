223fcc6c82618d1c552fd10a93839240cdadfcd3
Pinheiro <46682218+p1nh3iro@users.noreply.github.com>
2020-06-13 00:51:12 +0100

    Criação do fragmento para a medição de niveis de glicemia Inserção do form e icon para o respetivo efeito

---
+++ app/src/main/java/com/example/android/diadata/ui/NewMeasurement.java
@@ -0,0 +1,25 @@
+package com.example.android.diadata.ui;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.android.diadata.R;
+
+public class NewMeasurement extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.new_measurement, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
